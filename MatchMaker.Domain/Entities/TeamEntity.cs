e3c3350308f324b0266cc45848f19806c996ccfc
Per Karlsson <per.martin.karlsson@gmail.com>
2025-03-02 21:40:09 +0100

    Created initial user-repo with Crud-functionality.

--- MatchMaker.Domain/Entities/TeamEntity.cs
+++ MatchMaker.Domain/Entities/TeamEntity.cs
@@ -1,8 +1,7 @@
 ﻿using MongoDB.Entities;
 
 namespace MatchMaker.Domain.Entities;
-
-class TeamEntity : Entity
+public class TeamEntity : Entity
 { 
     public string TeamName { get; set; } = null!;
     public List<UserEntity> TeamCoaches { get; set; } = new();
