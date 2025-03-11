882b3ba3a4e50938a27bf6622d7af9adf589cb34
Per Karlsson <per.karlsson@yh.nackademin.se>
2025-03-11 23:26:24 +0100

    Implementation of result-class and ProblemDetails for better success and failure-handling.

---
+++ MatchMaker.Core/Utilities/Result.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchMaker.Domain.DTOs;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; } = null!;
+    public T? Data { get; set; }
+    public static Result<T> Success(T data, string message = "") => new Result<T> { IsSuccess = true, Data = data, Message = message };
+    public static Result<T> Failure(string message) => new Result<T> { IsSuccess = false, Message = message };
+
+}
