a58105f9a2dacda6c9d9b0e191f956c0b6b5be3f
Per Karlsson <per.karlsson@yh.nackademin.se>
2025-03-16 19:23:20 +0100

    Changes to how the SMTP-client handles configuration.

--- MatchMaker.Api/Extensions/SmtpClientServiceExtension.cs
+++ MatchMaker.Api/Extensions/SmtpClientServiceExtension.cs
@@ -8,21 +8,12 @@ namespace MatchMaker.Api.Extensions;
 
 public static class SmtpClientServiceExtension
 {
-    public static IServiceCollection AddSmtpServices(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddSmtpServices(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment env)
     {
         services.Configure<SmtpSettings>(configuration.GetSection("SmtpSettings"));
-        services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
-            
-        services.AddSingleton<IEmailTemplateService, EmailTemplateService>();
-        services.AddSingleton<SmtpClient>(options =>
-            {
-                var smtpSettings = options.GetRequiredService<IOptions<SmtpSettings>>().Value;
-                var smtpClient = new SmtpClient();
-                smtpClient.Connect(smtpSettings.Host, smtpSettings.Port, smtpSettings.EnableSsl);
-                smtpClient.Authenticate(smtpSettings.Username, smtpSettings.Password);
 
-                return smtpClient;
-            });
+        var clientURL = env.EnvironmentName == "Development" ? configuration.GetValue<string>("FrontendClient:DevelopmentURL") : configuration.GetValue<string>("FrontendClient:ProductionURL");
+        if (clientURL == null) throw new InvalidOperationException("Frontend URL is not configured.");
 
         return services;
     }
--- MatchMaker.Domain/Configurations/SmtpSettings.cs
+++ MatchMaker.Domain/Configurations/SmtpSettings.cs
@@ -2,10 +2,9 @@
 
 public class SmtpSettings
 {
-    public string Host { get; set; } = null!;
-    public int Port { get; set; }
-    public string Username { get; set; } = null!;
-    public string Password { get; set; } = null!;
-    public string FromEmail { get; set; } = null!;
-    public bool EnableSsl { get; set; }
+    public string SmtpHost { get; set; } = null!;
+    public int SmtpPort { get; set; }
+    public string SmtpUsername { get; set; } = null!;
+    public string SmtpPassword { get; set; } = null!;
+    public string SmtpFromEmail { get; set; } = null!;
 }
