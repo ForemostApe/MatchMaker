53520b6a10c603988ee300bc5d60ef5d63458edc
Per Karlsson <per.karlsson@yh.nackademin.se>
2025-03-16 20:36:25 +0100

    Fixes to login-endpoint where the wrong object was being passed.

--- MatchMaker.Domain/DTOs/LoginDTO.cs
+++ MatchMaker.Domain/DTOs/LoginDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MatchMaker.Domain.DTOs;
+﻿namespace MatchMaker.Domain.DTOs;
 
 public class LoginDTO
 {
