70b4de096e91c2b4fe5c535cab8f8b033ca25d2e
Per Karlsson <per.karlsson@yh.nackademin.se>
2025-05-19 14:59:48 +0200

    Added MongoDb-annotations to object-classes.

--- MatchMaker.Domain/Entities/Team.cs
+++ MatchMaker.Domain/Entities/Team.cs
@@ -1,6 +1,11 @@
-﻿namespace MatchMaker.Domain.Entities;
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MatchMaker.Domain.Entities;
 public class Team : SchemaBase<Team>
-{ 
+{
+    [BsonElement("teamName")]
     public string TeamName { get; set; } = null!;
+
+    [BsonElement("teamLogo")]
     public string TeamLogo { get; set; } = null!;
 }
