32603c97e0cd316cd920ee0e2fa2dd99a0055ac8
Per <131469670+ForemostApe@users.noreply.github.com>
2025-05-19 15:00:03 +0200

    Added MongoDb-annotations to object-classes. (#146)

--- MatchMaker.Domain/Entities/User.cs
+++ MatchMaker.Domain/Entities/User.cs
@@ -15,16 +15,29 @@ public enum UserRole
 
 public class User : SchemaBase<User>
 {
+    [BsonElement("passwordHash")]
     public string PasswordHash { get; set; } = null!;
+
+    [BsonElement("email")]
     public string Email { get; set; } = null!;
+
+    [BsonElement("firstName")]
     public string FirstName { get; set; } = null!;
+
+    [BsonElement("lastName")]
     public string LastName { get; set; } = null!;
+
+    [BsonElement("createdDate")]
     public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
     
     [BsonRepresentation(BsonType.String)]
+    [BsonElement("userRole")]
     public UserRole UserRole { get; set; } = UserRole.Unspecified;
     
     [BsonRepresentation(BsonType.ObjectId)]
+    [BsonElement("teamAffiliation")]
     public string? TeamAffiliation { get; set; }
+
+    [BsonElement("isVerified")]
     public bool IsVerified { get; set; } = false;
 }
