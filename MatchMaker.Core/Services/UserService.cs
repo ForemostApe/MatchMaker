b64c1946524f5a8c5c0d81ce51a02d7fbf284531
Per Karlsson <per.karlsson@yh.nackademin.se>
2025-05-19 11:48:21 +0200

    Applied exception-handling to UserService.

--- MatchMaker.Core/Services/UserService.cs
+++ MatchMaker.Core/Services/UserService.cs
@@ -14,52 +14,91 @@ public class UserService(ILogger<UserService> logger, IUserRepo userRepo) : IUse
     public async Task<Result<User>> CreateUserAsync(User newUser)
     {
         ArgumentNullException.ThrowIfNull(newUser);
-            
-        var result = await _userRepo.CreateUserAsync(newUser);
-        return result != null 
-            ? Result<User>.Success(result, "User successfully created")
-            : Result<User>.Failure("Couldn't create user");
+
+        try
+        {
+            var result = await _userRepo.CreateUserAsync(newUser);
+            return result != null
+                ? Result<User>.Success(result, "User successfully created")
+                : Result<User>.Failure("Couldn't create user");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occured in the service-layer trying to create user.");
+            throw;
+        }
     }
 
     public async Task<Result<User>> GetUserByEmailAsync(string email)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(email);
 
-        var user = await _userRepo.GetUserByEmailAsync(email);
-        return user != null 
-            ? Result<User>.Success(user, "User successfully found.") 
-            : Result<User>.Failure($"Couldn't find user with email-address {email}"); ;
+        try { 
+            var user = await _userRepo.GetUserByEmailAsync(email);
+            return user != null 
+                ? Result<User>.Success(user, "User successfully found.") 
+                : Result<User>.Failure($"Couldn't find user with email-address {email}");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occured in the service-layer trying to find user by email-address.");
+            throw;
+        }
 }
 
     public async Task<Result<User>> GetUserByIdAsync(string userId)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(userId);
 
-        var user = await _userRepo.GetUserByIdAsync(userId);
-        return user != null 
-            ? Result<User>.Success(user, "User successfully found.") 
-            : Result<User>.Failure("Failed to find user.");
+        try 
+        {
+            var user = await _userRepo.GetUserByIdAsync(userId);
+            return user != null
+                ? Result<User>.Success(user, "User successfully found.")
+                : Result<User>.Failure("Failed to find user.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occured in the service-layer trying to find user by UserId.");
+            throw;
+        }
     }
 
     public async Task<Result<List<User>>> GetUsersByRoleAsync(UserRole parsedRole, string? teamId = null)
     {
-        var users = await _userRepo.GetUsersByRole(parsedRole, teamId);
-        return users.Count > 0 
-            ? Result<List<User>>.Success(users, $"Users with role {parsedRole} successfully found.") 
-            : Result<List<User>>.Failure($"Failed to find any users with the role {parsedRole}" + (teamId != null ? $" in team {teamId}" : ""));
+        try
+        {
+            var users = await _userRepo.GetUsersByRole(parsedRole, teamId);
+            return users.Count > 0
+                ? Result<List<User>>.Success(users, $"Users with role {parsedRole} successfully found.")
+                : Result<List<User>>.Failure($"Failed to find any users with the role {parsedRole}" + (teamId != null ? $" in team {teamId}" : ""));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occured in the service-layer trying to find user by UserRole.");
+            throw;
+        }
     }
 
     public async Task<Result<User>> UpdateUserAsync(User updatedUser)
     {
         ArgumentNullException.ThrowIfNull(updatedUser);
 
-        var result = await _userRepo.UpdateUserAsync(updatedUser);
+        try 
+        { 
+            var result = await _userRepo.UpdateUserAsync(updatedUser);
 
-        if (result.MatchedCount > 0 && result.ModifiedCount == 0) return Result<User>.Success(updatedUser, "User already updated.");
+            if (result.MatchedCount > 0 && result.ModifiedCount == 0) return Result<User>.Success(updatedUser, "User already updated.");
         
-        return result.ModifiedCount > 0 
-            ? Result<User>.Success(updatedUser, "User successully updated.") 
-            : Result<User>.Failure("An error occurred trying to update user.");
+            return result.ModifiedCount > 0 
+                ? Result<User>.Success(updatedUser, "User successully updated.") 
+                : Result<User>.Failure("An error occurred trying to update user.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occured in the service-layer trying to find update user.");
+            throw;
+        }
     }
      
 
@@ -67,19 +106,35 @@ public class UserService(ILogger<UserService> logger, IUserRepo userRepo) : IUse
     {
         ArgumentNullException.ThrowIfNull(verifiedUser);
 
-        var result = await _userRepo.VerifyEmailAsync(verifiedUser);
-        return result.ModifiedCount > 0 
-            ? Result<User>.Success(verifiedUser, "Email-address successfully verified.")
-            : Result<User>.Failure("Failed to verify email-address.");
+        try
+        {
+            var result = await _userRepo.VerifyEmailAsync(verifiedUser);
+            return result.ModifiedCount > 0
+                ? Result<User>.Success(verifiedUser, "Email-address successfully verified.")
+                : Result<User>.Failure("Failed to verify email-address.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occured in the service-layer trying to find verify user-email.");
+            throw;
+        }
     }
 
     public async Task<Result<User>> DeleteUserAsync(string userId)
     {
         ArgumentNullException.ThrowIfNull(userId);
 
-        var result = await _userRepo.DeleteUserAsync(userId);
-        return result.DeletedCount > 0 
-            ? Result<User>.Success(null, "Successfully deleted user.")
-            : Result<User>.Failure("Failed to delete user.");
+        try
+        {
+            var result = await _userRepo.DeleteUserAsync(userId);
+            return result.DeletedCount > 0
+                ? Result<User>.Success(null, "Successfully deleted user.")
+                : Result<User>.Failure("Failed to delete user.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occured in the service-layer trying to delete user.");
+            throw;
+        }
     }
 }
