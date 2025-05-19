d59735db5f98b190b3958ea02f754e9964be6988
Per Karlsson <per.karlsson@yh.nackademin.se>
2025-05-19 12:37:33 +0200

    Added exception-handling to TeamServiceFacade.

--- MatchMaker.Core/Facades/TeamServiceFacade.cs
+++ MatchMaker.Core/Facades/TeamServiceFacade.cs
@@ -4,11 +4,13 @@ using MatchMaker.Core.Interfaces;
 using MatchMaker.Domain.DTOs;
 using MatchMaker.Domain.DTOs.Teams;
 using MatchMaker.Domain.Entities;
+using Microsoft.Extensions.Logging;
 
 namespace MatchMaker.Core.Facades;
 
-public class TeamServiceFacade(IMapper mapper, ITeamService teamService) : ITeamServiceFacade
+public class TeamServiceFacade(ILogger<TeamServiceFacade> logger, IMapper mapper, ITeamService teamService) : ITeamServiceFacade
 {
+    private readonly ILogger<TeamServiceFacade> _logger = logger;
     private readonly IMapper _mapper = mapper;
     private readonly ITeamService _teamService = teamService;
 
@@ -16,69 +18,117 @@ public class TeamServiceFacade(IMapper mapper, ITeamService teamService) : ITeam
     {
         ArgumentNullException.ThrowIfNull(newTeam);
 
-        var team = _mapper.Map<Team>(newTeam);
-        var result = await _teamService.CreateTeamAsync(team);
+        try
+        {
+            var team = _mapper.Map<Team>(newTeam);
+            var result = await _teamService.CreateTeamAsync(team);
 
-        return result.IsSuccess 
-            ? Result<TeamDTO>.Success(_mapper.Map<TeamDTO>(result.Data!), result.Message)
-            : Result<TeamDTO>.Failure(result.Message);
+            return result.IsSuccess
+                ? Result<TeamDTO>.Success(_mapper.Map<TeamDTO>(result.Data!), result.Message)
+                : Result<TeamDTO>.Failure(result.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unexpected error occurred in TeamServiceFacade while trying create team.");
+            throw;
+        }
     }
 
     public async Task<Result<List<TeamDTO>>> GetAllTeamsAsync()
     {
-        var result = await _teamService.GetAllTeamsAsync();
+        try
+        {
+            var result = await _teamService.GetAllTeamsAsync();
 
-        return result.IsSuccess 
-            ? Result<List<TeamDTO>>.Success(_mapper.Map<List<TeamDTO>>(result.Data!), result.Message) 
-            : Result<List<TeamDTO>>.Failure(result.Message);
+            return result.IsSuccess
+                ? Result<List<TeamDTO>>.Success(_mapper.Map<List<TeamDTO>>(result.Data!), result.Message)
+                : Result<List<TeamDTO>>.Failure(result.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unexpected error occurred in TeamServiceFacade while trying get all teams.");
+            throw;
+        }
     }
 
     public async Task<Result<TeamDTO>> GetTeamByIdAsync(string teamId)
     {
         ArgumentException.ThrowIfNullOrEmpty(teamId);
 
-        var result = await _teamService.GetTeamByIdAsync(teamId);
+        try
+        {
+            var result = await _teamService.GetTeamByIdAsync(teamId);
 
-        return result.IsSuccess 
-            ? Result<TeamDTO>.Success(_mapper.Map<TeamDTO>(result.Data!), result.Message) 
-            : Result<TeamDTO>.Failure(result.Message);
+            return result.IsSuccess
+                ? Result<TeamDTO>.Success(_mapper.Map<TeamDTO>(result.Data!), result.Message)
+                : Result<TeamDTO>.Failure(result.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unexpected error occurred in TeamServiceFacade while trying get team by TeamId.");
+            throw;
+        }
     }
 
     public async Task<Result<TeamDTO>> GetTeamByNameAsync(string teamName)
     {
         ArgumentException.ThrowIfNullOrEmpty(teamName);
 
-        var result = await _teamService.GetTeamByNameAsync(teamName);
+        try
+        {
+            var result = await _teamService.GetTeamByNameAsync(teamName);
 
-        return result.IsSuccess 
-            ? Result<TeamDTO>.Success(_mapper.Map<TeamDTO>(result.Data!), result.Message)
-            : Result<TeamDTO>.Failure(result.Message);
+            return result.IsSuccess
+                ? Result<TeamDTO>.Success(_mapper.Map<TeamDTO>(result.Data!), result.Message)
+                : Result<TeamDTO>.Failure(result.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unexpected error occurred in TeamServiceFacade while trying get team by TeamName.");
+            throw;
+        }
     }
 
     public async Task<Result<TeamDTO>> UpdateTeamAsync(UpdateTeamDTO updatedTeam)
     {
         ArgumentNullException.ThrowIfNull(updatedTeam);
 
-        var existingTeam = await _teamService.GetTeamByIdAsync(updatedTeam.TeamId);
-        if (existingTeam == null) return Result<TeamDTO>.Failure("Coulnd't find the specified team.");
+        try
+        {
+            var existingTeam = await _teamService.GetTeamByIdAsync(updatedTeam.TeamId);
+            if (existingTeam == null) return Result<TeamDTO>.Failure("Coulnd't find the specified team.");
 
-        updatedTeam.Adapt(existingTeam.Data!);
+            updatedTeam.Adapt(existingTeam.Data!);
 
-        var result = await _teamService.UpdateTeamAsync(existingTeam.Data!);
+            var result = await _teamService.UpdateTeamAsync(existingTeam.Data!);
 
-        return result.IsSuccess 
-            ? Result<TeamDTO>.Success(result.Data!.Adapt<TeamDTO>(), result.Message)
-            : Result<TeamDTO>.Failure(result.Message);
+            return result.IsSuccess
+                ? Result<TeamDTO>.Success(result.Data!.Adapt<TeamDTO>(), result.Message)
+                : Result<TeamDTO>.Failure(result.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unexpected error occurred in TeamServiceFacade while trying update team.");
+            throw;
+        }
     }
 
     public async Task<Result<TeamDTO>> DeleteTeamAsync(DeleteTeamDTO deleteTeamDTO)
     {
         ArgumentNullException.ThrowIfNull(deleteTeamDTO);
 
-        var result = await _teamService.DeleteTeamAsync(deleteTeamDTO.TeamId);
+        try
+        {
+            var result = await _teamService.DeleteTeamAsync(deleteTeamDTO.TeamId);
 
-        return result.IsSuccess ?
-            Result<TeamDTO>.Success(null, result.Message)
-            : Result<TeamDTO>.Failure(result.Message);
+            return result.IsSuccess ?
+                Result<TeamDTO>.Success(null, result.Message)
+                : Result<TeamDTO>.Failure(result.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unexpected error occurred in TeamServiceFacade while trying delete team.");
+            throw;
+        }
     }
 }
